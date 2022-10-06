b517344b172beab185e2b5c8b1339aac7967947f
Alex Soler PAV ETSETB <alex.soler.baca@estudiantat.upc.edu>
2022-10-06 22:15:36 +0200

    Ficheros con autput P1

--- p1.c
+++ p1.c
@@ -8,15 +8,20 @@
 int main(int argc, char *argv[]) {
     float durTrm = 0.010;
     float fm;
-    int N;
-    int trm;
+    int   N;
+    int   trm;
     float *x;
     short *buffer;
-    FILE *fpWave;
+    FILE  *fpWave;
+    FILE  *fpOut;
 
     if (argc != 2 && argc != 3) {
         fprintf(stderr, "Empleo: %s inputfile [outputfile]\n", argv[0]);
         return -1;
+    }
+    
+    if (argc==3) {
+        fpOut = fopen(argv[2], "wt");
     }
 
     if ((fpWave = abre_wave(argv[1], &fm)) == NULL) {
@@ -29,6 +34,16 @@ int main(int argc, char *argv[]) {
         (x = malloc(N * sizeof(*x))) == 0) {
         fprintf(stderr, "Error al ubicar los vectores (%s)\n", strerror(errno));
         return -1;
+    }
+
+    trm = 0;
+    while (lee_wave(buffer, sizeof(*buffer), N, fpWave) == N) {
+        for (int n = 0; n < N; n++) x[n] = buffer[n] / (float) (1 << 15);
+    
+        fprintf(fpOut, "%d\t %f\t %f\t %f\n", trm, compute_power(x, N),
+                                        compute_am(x, N),
+                                        compute_zcr(x, N, fm));
+        trm += 1;
     }
 
     trm = 0;
