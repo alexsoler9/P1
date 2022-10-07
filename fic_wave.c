db3d76ed026e1f4344186dfa19ce962aa09c2000
Alex Soler PAV ETSETB <alex.soler.baca@estudiantat.upc.edu>
2022-10-07 11:01:43 +0200

    Cambios fic_wave, leactura fm

--- fic_wave.c
+++ fic_wave.c
@@ -3,11 +3,14 @@
 
 FILE *abre_wave(const char *ficWave, float *fm) {
     FILE *fpWave;
+    unsigned int fmHeader; //no puede ser negativa
 
     if ((fpWave = fopen(ficWave, "r")) == NULL) return NULL;
     if (fseek(fpWave, 44, SEEK_SET) < 0) return NULL;
 
-    *fm = 16000;
+    fseek(fpWave, 24, SEEK_SET); //desde el principio del fichero, fm corresponde a un offset de 24
+    fread(&fmHeader, 4, 1, fpWave); //fm tiene un tamaño de 4 bytes
+    *fm = fmHeader;
 
     return fpWave;
 }
